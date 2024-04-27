ea9dbc380be949a680c329df9ed57188b46b5a7f
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-04-27 17:35:11 +0200

    Added handler channel link.

--- back-bot/main.py
+++ back-bot/main.py
@@ -26,7 +26,18 @@ def set_user_state(user_id, state):
 # Function to get user state
 def get_user_state(user_id):
     return user_states.get(user_id)
+def reset_user_state(user_id):
+    if user_id in user_states:
+        del user_states[user_id]
 
+def is_bot_admin(message):
+    try:
+        channel_id = client.get_chat(message.text).id
+        chat_member = client.get_chat_member(channel_id, client.get_me().id)
+        return chat_member.status == "administrator" or chat_member.status == "creator"
+    except Exception as e:
+        print("Error:", e)
+        return False
 # commands
 @client.message_handler(commands=["start"])
 def start_command(message):
@@ -40,14 +51,20 @@ def auction_command(message):
 
 @client.message_handler(commands=["create_auction"])
 def create_auction_command(message):
-    client.send_message(message.chat.id, "You want to create auction? Let's start. Please add me to your channel and make me admin. Then send me the link of your channel.")
+    client.send_message(message.chat.id, "You want to create auction? Let's start. Please add me to your channel and make me admin. Then send me the link of your channel (@name).")
+    set_user_state(message.chat.id, "awaiting_channel_link")
 
 @client.message_handler(commands=["quit"])
 def quit_command(message):
     reset_user_state(message.chat.id)
     client.send_message(message.chat.id, "You have left all ongoing commands.")
 
-# Callbacks
-
+@client.message_handler(func=lambda message: get_user_state(message.chat.id) == "awaiting_channel_link")
+def handle_channel_link(message):
+    while not is_bot_admin(message):
+        client.send_message(message.chat.id, "I am not an admin in the channel. Please add me as an admin and try again.")
+        return
+    set_user_state(message.chat.id, "awaiting_channel_description")
+    client.send_message(message.chat.id, "Send me description of your channel.")
 
 client.infinity_polling()
