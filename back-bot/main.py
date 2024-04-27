19a00de693117a678574e3aa63044b35bf15bea1
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-04-27 17:02:48 +0200

    Setup webapp with localhost of dan pc tailscale

--- back-bot/main.py
+++ back-bot/main.py
@@ -5,12 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("SECRET_KEY")
+# frontUrl = os.getenv("FRONT_URL")
+frontUrl = "https://100.110.247.101:3000/"
 client = telebot.TeleBot(TOKEN)
 
 client.set_my_commands([
     telebot.types.BotCommand("start", "start of the bot."),
     telebot.types.BotCommand("auction", "auction of the channels and chat's."),
     telebot.types.BotCommand("create_auction", "Create auction of your channel."),
+    telebot.types.BotCommand("quit", "Leave all ongoing commands.")
 ])
 
 # commands
@@ -19,9 +22,9 @@ def start_command(message):
     client.send_message(message.chat.id, "Hello! To create your auction you can use command - /create_auction\nTo check all active auctions command - /auction")
 @client.message_handler(commands=["auction"])
 def auction_command(message):
-    markup = telebot.types.InlineKeyboardMarkup()
-    WebApp = telebot.types.WebAppData("https://adsstorage.herokuapp.com/auctions")
-    markup.add(telebot.types.InlineKeyboardButton("See all auctions", web_app=WebApp))
+    markup = telebot.types.InlineKeyboardMarkup(row_width=2)
+    WebApp = telebot.types.WebAppInfo(frontUrl)
+    markup.add(telebot.types.InlineKeyboardButton(text="See all auctions", web_app=WebApp))
     client.send_message(message.chat.id, "See all auctions.", reply_markup=markup)
 
 @client.message_handler(commands=["create_auction"])
